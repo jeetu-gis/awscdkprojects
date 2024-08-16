eb0935191c8d80b6694972beaae81969ab212248
jeetu-gis <jeetu-gis@users.noreply.github.com>
2024-08-16 10:48:00 -0500

    fix

--- serverless/lib/serverless-stack.ts
+++ serverless/lib/serverless-stack.ts
@@ -26,7 +26,7 @@ import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { RetentionDays } from "aws-cdk-lib/aws-logs";
 import path = require("path");
 import { ExecSyncOptions, execSync } from "child_process";
-import { S3 } from "aws-cdk-lib/aws-ses-actions";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 import { CacheControl } from "aws-cdk-lib/aws-codepipeline-actions";
 import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
